dabfdb63fb0b940deb39c878d9bc734e90a1ef18
Bishakha Roy <broy9813@gmail.com>
2019-10-08 10:56:01 -0400

    This is the 3rd version of my reminder.

--- app/src/main/java/com/example/myreminders/AddReminders.java
+++ app/src/main/java/com/example/myreminders/AddReminders.java
@@ -7,25 +7,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class AddReminders extends CursorAdapter {
 
     public AddReminders(Context context, Cursor cursor, int flags) {
         super(context, cursor, flags);
     }
+
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        return LayoutInflater.from(context).inflate(R.layout.li_shopping_list, viewGroup, false);
+        return LayoutInflater.from(context).inflate(R.layout.li_addreminder_list, viewGroup, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ((TextView) view.findViewById(R.id.titleTextView)).
-                setText(cursor.getString(cursor.getColumnIndex( "title")));
+                setText(cursor.getString(cursor.getColumnIndex("title")));
         ((TextView) view.findViewById(R.id.dateTextView)).
-                setText(cursor.getString(cursor.getColumnIndex( "date")));
-        ((TextView) view.findViewById(R.id.typeTextView)).
-                setText(cursor.getString(cursor.getColumnIndex( "type")));
+                setText(cursor.getString(cursor.getColumnIndex("date")));
+        //get the reminder date and store it in variable
+        String reminderdate = cursor.getString(cursor.getColumnIndex("date"));
+        //getting the current date
+        Date currentdate = new Date();
+        //converting string to date
+        try {
+            //converting reminderdate to a string
+            Date newreminderdate = new SimpleDateFormat("yyyy-MM-dd").parse(reminderdate);
+            if(currentdate.compareTo(newreminderdate) <0) {
+                ((TextView) view.findViewById(R.id.dateexpiredTextView)).
+                        setText(("Expired ? True"));
+            } else if (newreminderdate.compareTo(currentdate) < 0) {
+                ((TextView) view.findViewById(R.id.dateexpiredTextView)).
+                        setText(("Expired ? False"));
+
+            }
+        } catch (Exception e) {
+        }
+
+
+        }
     }
-}
+
 
--- app/src/main/java/com/example/myreminders/MainActivity.java
+++ app/src/main/java/com/example/myreminders/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.myreminders;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     DBHandler dbHandler;
 
     //declare a Shopping Lists Cursor Adapter
-    myReminderList myReminderListAdapter;
+    AddReminder addReminderAdapter;
 
     //declare a ListView
     ListView myReminderListView;
@@ -67,5 +72,46 @@ public class MainActivity extends AppCompatActivity {
     public void openCreateList(View view) {
         intent = new Intent(this, CreateList.class );
         startActivity(intent);
+    }
+
+    public void updateItem(long id){
+
+        // if shopping list item's has indicator isn't true
+        if (dbHandler.isReminderAdded((int)id) != 0){
+
+            // update shopping list item's has indicator to true
+            dbHandler.updateReminder
+            // display toast indicating shopping list item has been purchased
+            Toast.makeText(this, "Reminder Added!", Toast.LENGTH_LONG).show();
+
+            // refresh shopping list item in the ListView -
+            // change the Item Purchased from false to true
+            addReminderAdapter.swapCursor(dbHandler.getaddReminder((int)this.id));
+            addReminderAdapter.notifyDataSetChanged();
+        }
+
+        // if all shopping list items have been purchased
+        if (dbHandler.getedremindernotadded((int)this.id) == 0){
+
+            //initialize Notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+            //set it icon, title, and text
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            builder.setContentTitle("MyReminder");
+            builder.setContentText("Reminder is Added!");
+
+            //initilize qa PendingIntent
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            //set the content intent of the Notification
+            builder.setContentIntent(pendingIntent);
+
+            //initialize a NotificationManger
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            //have the NotificationManager send the Notification
+            notificationManager.notify(2142, builder.build());
+        }
     }
 }
